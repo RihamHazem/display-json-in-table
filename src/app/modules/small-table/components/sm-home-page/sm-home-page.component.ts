8e5bfe933b3782515aae66d0411492a085728c39
RihamHazem <rihamhazem56@gmail.com>
2018-09-26 18:17:25 +0200

    modifying filters

--- src/app/modules/small-table/components/sm-home-page/sm-home-page.component.ts
+++ src/app/modules/small-table/components/sm-home-page/sm-home-page.component.ts
@@ -8,7 +8,6 @@ import { GetJsonService } from '../../../../shared/get-json.service';
 })
 export class SmHomePageComponent implements OnInit {
   // it contains all rows for the shown columns ["Tests", "Status"]
-  tableData: any[] = [];
   myTableData = {};
   // it contains the columns that being displayed in the table
   columnNames: string[] = ["Tests"];
@@ -49,11 +48,7 @@ export class SmHomePageComponent implements OnInit {
           }
           let exec_status = curData[j]['exec_state'];
           let f_status = curData[j]['fstatus'];
-          if (curData[j]['fstatus'] === null || curData[j]['fstatus'] === undefined) {
-            f_status = "---";
-          } else {
-            f_status = this.getUniqueSubSentences(curData[j]['fstatus']);
-          }
+          f_status = this.getUniqueSubSentences(curData[j]['fstatus']);
           this.myTableData[test_name][i] = {
             'Status': exec_status,
             'fStatus': f_status
@@ -66,13 +61,6 @@ export class SmHomePageComponent implements OnInit {
             this.map_status[exec_status] = 1;
             // marks the rows that have all values of status as visible
             this.statusVisibility[exec_status] = true;
-          }
-          if (this.map_fStatus.hasOwnProperty(f_status)) {
-            // it counts the fStatus value so that it would be used later for filtering the row's of the table
-            this.map_fStatus[f_status]++;
-          } else {
-            this.map_fStatus[f_status] = 1;
-            this.fStatusVisibility[f_status] = true;
           }
 
           // then it's pushed to allTableData Object
@@ -95,88 +83,52 @@ export class SmHomePageComponent implements OnInit {
           once = false;
         }
       }
-    });
-  }
-  constructTable() {
-    this.allTableData = {};
-    this.allColumnData = [];
-    this._getJsonService.getJsonSmallTable().subscribe(data => {
-      let ourData = data[0]['test_instances'];
-      let once = true;
-      let oneTime = true;
-      // extracts from the JSON only three columns from the test instances's key: "test name", "exec state" and "f status"
-      for (let index in ourData) {
-        let test_name: string = ourData[index]['test_name'];
-        this.map_test_name[test_name] = index;
-        let exec_state: string[] = [];
-        let f_status: string[] = [];
-        for (let i in data) {
-          let cur_exec_status = data[i]['test_instances'][index]['exec_state'];
-          exec_state.push(cur_exec_status);
-          if (this.map_status.hasOwnProperty(cur_exec_status)) {
-            // it counts the status value so that it would be used later for filtering the row's of the table
-            this.map_status[cur_exec_status]++;
-          } else {
-            // it sets the status value to 1.
-            this.map_status[cur_exec_status] = 1;
-            // marks the rows that have all values of status as visible
-            this.statusVisibility[cur_exec_status] = true;
-          }
-
-          let cur_fStatus;
-          if (data[i]['test_instances'][index]['fstatus'] === null || data[i]['test_instances'][index]['fstatus'] === undefined) {
-            cur_fStatus = "---";
-            f_status.push(cur_fStatus);
-          } else {
-            cur_fStatus = this.getUniqueSubSentences(data[i]['test_instances'][index]['fstatus']);
-            f_status.push(cur_fStatus);
-          }
-          if (this.map_fStatus.hasOwnProperty(cur_fStatus)) {
-            // it counts the fStatus value so that it would be used later for filtering the row's of the table
-            this.map_fStatus[cur_fStatus]++;
-          } else {
-            this.map_fStatus[cur_fStatus] = 1;
-            this.fStatusVisibility[cur_fStatus] = true;
-          }
-          if (oneTime)
-            this.columnNames.push("Status");
-        }
-        oneTime = false;
-        // it pushes the collected data at the tableData object
-        this.tableData.push({
-          'Tests': test_name,
-          'Status': exec_state,
-          'fStatus': f_status
-        });
-        // saves also all the data of all columns to be used in showing more info about the row
-        // then it's pushed to allTableData Object
-        this.allTableData[test_name] = [];
-        for (let i in data) {
-          let temp = {};
-          for (let key in data[i]['test_instances'][index]) {
-            if (key === 'id' || key === 'test_name' || key === 'test_id') {
-              continue;
+      for (let key in this.myTableData) {
+        let curRow = this.myTableData[key];
+        for (let index = 0; index < curRow.length; index++) {
+          if (curRow[index] === undefined || curRow[index] === null) {
+            if (!this.map_fStatus.hasOwnProperty("---")) {
+              this.map_fStatus["---"] = 0;
             }
-            temp[key] = data[i]['test_instances'][index][key];
-            if (once) {
-              // saves all the columns
-              this.allColumnData.push(key);
+            if (this.map_status.hasOwnProperty("NO STATUS")) {
+              this.map_status["NO STATUS"]++;
+              this.map_fStatus["---"]++;
+              curRow[index] = {
+                "Status": "NO STATUS",
+                "fStatus": ["---"]
+              };
+            } else {
+              this.map_status["NO STATUS"] = 1;
+              this.map_fStatus["---"]++;
+              // marks the rows that have all values of status as visible
+              this.statusVisibility["NO STATUS"] = true;
+              curRow[index] = {
+                "Status": "NO STATUS",
+                "fStatus": ["---"]
+              };
             }
           }
-          this.allTableData[test_name].push(temp);
-          once = false;
         }
       }
     });
   }
 
   public getUniqueSubSentences(str: string) {
+    if (str === undefined || str === null) {
+      str = "---";
+    }
     let splitter = str.split(',');
     let mp = {};
     let ans: string[] = [];
     for (let i in splitter) {
       if (!mp.hasOwnProperty(splitter[i])) {
-        ans.push(" " + splitter[i]);
+        ans.push(splitter[i]);
+        if (this.map_fStatus.hasOwnProperty(splitter[i])) {
+          this.map_fStatus[splitter[i]]++;
+        } else {
+          this.map_fStatus[splitter[i]] = 1;
+          this.fStatusVisibility[splitter[i]] = true;
+        }
       }
       mp[splitter[i]] = 1;
     }
--- src/app/modules/small-table/shared/sm-filter-table.pipe.ts
+++ src/app/modules/small-table/shared/sm-filter-table.pipe.ts
@@ -9,9 +9,13 @@ export class SmFilterTablePipe implements PipeTransform {
   transform(rows: any, rowVisibility): any {
     let newArr = [];
     for (let key in rows) {
-      let allTrue = true;
+      let allTrue = false;
       for (let index in rows[key]) {
-        allTrue = allTrue && rowVisibility["Status"][rows[key][index]["Status"]] && rowVisibility["fStatus"][rows[key][index]["fStatus"]] === true;
+        let subTrue = false;
+        for (let fs in rows[key][index]["fStatus"]) {
+          subTrue = subTrue || (rowVisibility["fStatus"][rows[key][index]["fStatus"][fs]] === true);
+        }
+        allTrue = allTrue || (rowVisibility["Status"][rows[key][index]["Status"]] === true && subTrue);
       }
       if (allTrue) {
         newArr.push({
